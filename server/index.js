ed147aecc68e7275c3f3fa14c6f4361938665fd6
rutu1518 <rutujamore1508@gmail.com>
2024-08-08 11:14:44 +0530

    server updated

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
-
-
 import { postLink, getRedirectlink , getLinks, deleteLink, updatelink} from "./controllers/link.js";
 import {postSignup, postLogin } from "./controllers/user.js";
 
@@ -13,28 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 const MongoDB = async () => {
-  try {
-    const conn = await mongoose.connect(`${process.env.MONGO_URL}`);
-    console.log(`MongoDB Connected Successfully`);
-  } catch (error) {
-    console.error(`Connection Failed`, error);
-  }
+ 
+    const conn = await mongoose.connect(process.env.MONGO_URL);    
+    if(conn){
+      console.log(`MongoDB Connected Successfully`);
+    }
 };
 MongoDB();
 
 app.get("/health", (req, res) => {
   res.json({
-    message: "Server is running",
     success: true,
-  });
-});
+    message: "Server is running"
+  })
+})
 
-app.post("/link", postLink);
-app.get("/Links", getLinks);
-app.get("/:slug", getRedirectlink);
-app.post("/signup", postSignup);
-app.post("/Login", postLogin);
-app.delete("/link/:id", deleteLink);
+app.post("/link", postLink)
+app.get("/Links", getLinks)
+app.get("/:slug", getRedirectlink)
+app.post("/signup", postSignup)
+app.post("/Login", postLogin)
+app.delete("/link/:id", deleteLink)
 app.put("/link/:id", updatelink )
 
 const PORT = process.env.PORT || 8000;
